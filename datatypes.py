565ebfae91e132b03ac216e5b434c7999066f390
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-04-30 10:56:41 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,31 @@
+# #string: which is surrounded by " " or ' ' 
+#data types in python
+# a=10 #interger
+# b=2.5 #float
+# c="Arun" #string/char
+# d=True #boolean/bool
+# e=None #none datatype
+# print(type(e))
+# print(type(d))
+
+#Python Datatypes
+
+#set
+# b={"kolkata", "mumbai", "kolkata", "pune",5}
+# print(b)
+# print(type(b))
+
+#List/ Array/ Arraylist
+# b=[5,6, "kolkata", "mumbai"]
+# print(b)
+# print(type(b))
+
+#tuple
+# b=(5,6,9,6,3,2,5)
+# print(b)
+# print(type(b))
+
+#Dictionary
+# b={"name":"amit","age":23,"profession":"developer"}
+# print(b["profession"])
+# print(type(b))
