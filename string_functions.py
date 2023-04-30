56f565b8f57f70a2cbc4fc7bf4b1c15a608b4211
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-04-30 10:56:59 +0530

    Add files via upload

---
+++ string_functions.py
@@ -0,0 +1,31 @@
+#string Slicing
+#indexing--> 0---N 
+# s="abcdefghijklmnopqrst"
+# # print(s[0:10]) #it will go to 0 to n-1
+# print(s[2:13])
+
+#upper case / lower case
+# a=" priyanka "
+# print(len(a))
+# # print(a.upper())
+# # print(a.lower())
+# # print(a.isupper())
+# # print(a.islower())
+# b=a.strip() # it will remove the white spaces from the beginning 
+#             #or from the end
+# print(len(b))
+#string concatination
+# a="kolkata "
+# b="biriyani"
+# print(a+"loves "+b)
+# a="priyanka"
+# print(a.capitalize())
+# b=[1,2,5,6,8]
+# b.pop(2)
+# print(b)
+
+#concatination
+# x="I am a student. And I"
+# y=" love"
+# z=" Python"
+# print(x+y+z)
