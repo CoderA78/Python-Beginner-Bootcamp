414cb3bd7a94c6a0572bbe1b3de775f3bb460d85
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-04-30 10:34:37 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,172 @@
+# print("Hello World")
+# #string: which is into the " " or ' ' 
+# # "#" is known as comment
+# print('Hello World')
+#jvkhsbdj sdk kjk kkj m k
+#we have use for loop till length of the arary
+#Vari ables:
+# vary: 5-10
+# x=10 
+# x=2.5 #the number which has point
+# # x=True
+# #variables: In which we can store any integers, float, character, 
+# # boolean 1 and 0
+# print(x)
+# sourav=10
+# Sourav=25
+# print(Sourav)
+# my_name_is_sourav=7
+# print(my_name_is_sourav)
+# sourav=10
+# sourav=7
+# print(sourav)
+# x= "Midnapore10"
+# x= "kolkata"
+# z = "dum dum"
+# print(x)
+# # print(y)
+# print(z)
+# a, a, b= "midnapore", "howrah", "park circus"
+#concatination
+# x="I am a student. And I"
+# y=" love"
+# z=" Python"
+# print(x+y+z)
+# a="kbnsdkjcnljabc 654651chbjhw876387223mnbjdj b"
+# b="mhsbcjsb sosdnjs8787scjn sc "
+# print(a+b)
+# x="5" #integer
+# y="10"
+# print("The sum of x and y is: ",x+y)
+# # print("my name is Anup", x)
+# # print(type(x))
+# # print(type(y))
+# 10+5=15
+# x=2.5 #float values
+# y=7 #integer values
+# print(type(x+y))
+# x=5 #integer
+# y="john" #string
+# print(x+y)
+#data types in python
+# a=10 #interger
+# b=2.5 #float
+# c="Arun" #string/ character
+# d=True #boolean/bool
+# e=None #none datatype
+# print(type(e))
+# print(type(d))
+#Python Datatypes
+#set
+# b={"kolkata", "mumbai", "kolkata", "pune",5}
+# print(b)
+# print(type(b))
+
+#List/ Array/ Arraylist
+# b=[5,6, "kolkata", "mumbai"]
+# print(b)
+# print(type(b))
+
+#tuple
+# b=(5,6,9,6,3,2,5)
+# print(b)
+# print(type(b))
+
+#Dictionary
+# b={"name":"amit","age":23,"profession":"developer"}
+# print(b["profession"])
+# print(type(b))
+
+#python casting
+# b="kolkata" #float
+# print(type(b))
+# c=int(b)
+# print(c)
+# print(type(c))
+# a="kolkata"
+# b="pune"
+# print(a,b)
+# a="bangalore"
+# a="park circus"
+# print(a,b)
+#string Slicing
+#indexing--> 0---N 
+# s="abcdefghijklmnopqrst"
+# # print(s[0:10]) #it will go to 0 to n-1
+# print(s[2:13])
+
+#upper case
+a=" priyanka "
+# print(len(a))
+# # print(a.upper())
+# # print(a.lower())
+# # print(a.isupper())
+# # print(a.islower())
+# b=a.strip() # it will remove the white spaces from the beginning 
+#             #or from the end
+# print(len(b))
+#string concatination
+# a="kolkata "
+# b="biriyani"
+# print(a+"loves "+b)
+# a="priyanka"
+# print(a.capitalize())
+# b=[1,2,5,6,8]
+# b.pop(2)
+# print(b)
+
+#Operators
+# a=10
+# b=3
+# print(a+b) #addition
+# print(a-b) #substraction
+# print(a*b) #multiplication
+# print(a/b) #devide
+# print(a**b) #power func
+# print(a//b) #vaagfol
+# print(a%b) #remainder
+
+#logical operators
+# and or not || && 
+
+# li=[1,2,5,6,8,5,6,5]
+# print(li[3])
+# print(li[6])
+# print(li[23])
+#     0      1      2 
+# li=["pinky","moly","poly"]
+# li.append(510)
+# print(li)
+#if-else
+# a=10
+# b=10
+# if a>b:
+#     print("a is greater than b")
+# elif a==b:
+#     print("a is equal to b")
+# else:
+#     print("b is greater than a")  
+# a=0
+# if a<0:
+#     print("a is negative")
+# elif a>0:
+#     print("a is positive")
+# else:
+#     print("a is neither -ve nor +ve")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
